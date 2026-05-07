861576ae530b08e21114595d33e0c6e3b8993076
NagiReddy555 <55519745+NagiReddy555@users.noreply.github.com>
2026-05-07 09:49:23 -0400

    Update greetingsLibrary.groovy

--- vars/greetingsLibrary.groovy
+++ vars/greetingsLibrary.groovy
@@ -1,3 +1,6 @@
-def call(){
-    echo ' This is from shared library '
+def greeting1(){
+    echo ' This is from shared library  greeting 1'
+}
+def greeting2(){
+    echo ' This is from shared library greeting 2'
 }
